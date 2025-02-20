83222194d8ff1c6fb763a4dfac63a095d62a391a
amamFz <ilhamfauzicom21@gmail.com>
2025-02-20 23:14:18 +0700

    Create video detail

--- app/course/course_video/[id]/page.tsx
+++ app/course/course_video/[id]/page.tsx
@@ -1,49 +1,15 @@
+import AccordionCourse from "@/components/Common/AccordionCourseContent";
 import VideoThumbnail from "../VideoThumbnail";
 import {
   Accordion,
-  AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
-import { PiPlayFill } from "react-icons/pi";
-const data = [
-  {
-    tag: "Overview",
-  },
-  {
-    tag: "Lessons",
-  },
-  {
-    tag: "Review",
-  },
-];
+import { dataButton, lessons } from "@/data/course";
 
-const lessons = [
-  {
-    title: "Setup Project",
-    time: "5:00",
-    playIcon: <PiPlayFill />,
-  },
-  {
-    title: "Setup Project",
-    time: "5:00",
-    playIcon: <PiPlayFill />,
-  },
-  {
-    title: "Setup Project",
-    time: "5:00",
-    playIcon: <PiPlayFill />,
-  },
-  {
-    title: "Setup Project",
-    time: "5:00",
-    playIcon: <PiPlayFill />,
-  },
-];
 const CourseVideo = () => {
   return (
     <>
@@ -64,109 +30,31 @@ const CourseVideo = () => {
               {/* overview */}
               <div className="mb-6 flex flex-row gap-4">
                 {" "}
-                {data.map((item, index) => {
+                {dataButton.map((data) => {
                   return (
-                    <Link key={index} href="#">
-                      <Button className="rounded-full">{item.tag}</Button>
+                    <Link key={data.id} href="#">
+                      <Button className="rounded-full">{data.tag}</Button>
                     </Link>
                   );
                 })}
               </div>
             </div>
             {/* sidebar course lesson */}
-            <div className="sticky top-0  w-full self-start lg:w-[30%]">
-              <div className="rounded-lg  p-4 shadow-md">
-                <Accordion type="single" collapsible>
-                  <AccordionItem value="1">
-                    <AccordionTrigger>
-                      <h2 className="mb-4 text-xl font-semibold text-primary">
-                        Course Lessons
-                      </h2>
-                      {/* <Link href="#">
-                        <span className="px-4">
-                          <PiCaretDownLight className="h-6 w-6" />
-                        </span>
-                      </Link> */}
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      <div>
-                        {lessons.map((lesson, index) => {
-                          return (
-                            <div
-                              key={index}
-                              className="flex items-center justify-between p-4"
-                            >
-                              <Link
-                                href="#"
-                                className="flex items-center gap-4"
-                              >
-                                <div className="flex h-6 w-6 items-center justify-center rounded-full bg-secondary md:h-10 md:w-10">
-                                  {" "}
-                                  <span className=" text-white">
-                                    {lesson.playIcon}
-                                  </span>
-                                </div>
-                                <h3 className="text-base font-semibold text-primary md:text-lg">
-                                  {lesson.title}
-                                </h3>
-                              </Link>
-                              <span className="font-semibold text-primary">
-                                {lesson.time}
-                              </span>
-                            </div>
-                          );
-                        })}
-                      </div>
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </div>
-              <div className="rounded-lg  p-4 shadow-md">
-                <Accordion type="single" collapsible>
-                  <AccordionItem value="1">
-                    <AccordionTrigger>
-                      <h2 className="mb-4 text-xl font-semibold text-primary">
-                        Course Lessons
-                      </h2>
-                      {/* <Link href="#">
-                        <span className="px-4">
-                          <PiCaretDownLight className="h-6 w-6" />
-                        </span>
-                      </Link> */}
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      <div>
-                        {lessons.map((lesson, index) => {
-                          return (
-                            <div
-                              key={index}
-                              className="flex items-center justify-between p-4"
-                            >
-                              <Link
-                                href="#"
-                                className="flex items-center gap-4"
-                              >
-                                <div className="flex h-6 w-6 items-center justify-center rounded-full bg-secondary md:h-10 md:w-10">
-                                  {" "}
-                                  <span className=" text-white">
-                                    {lesson.playIcon}
-                                  </span>
-                                </div>
-                                <h3 className="text-base font-semibold text-primary md:text-lg">
-                                  {lesson.title}
-                                </h3>
-                              </Link>
-                              <span className="font-semibold text-primary">
-                                {lesson.time}
-                              </span>
-                            </div>
-                          );
-                        })}
-                      </div>
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
-              </div>
+            <div className="sticky top-0 w-full self-start lg:w-[30%]">
+              {Array.from({ length: 2 }).map((_, index) => (
+                <div key={index} className="rounded-lg p-4 shadow-md mb-4">
+                  <Accordion type="single" collapsible>
+                    <AccordionItem value={`section-${index + 1}`}>
+                      <AccordionTrigger>
+                        <h2 className="text-xl font-semibold">
+                          Course Section {index + 1}
+                        </h2>
+                      </AccordionTrigger>
+                      <AccordionCourse lesson={lessons} />
+                    </AccordionItem>
+                  </Accordion>
+                </div>
+              ))}
             </div>
           </div>
         </div>
